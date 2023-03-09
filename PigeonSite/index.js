f1ce738a57fe3092c7c488e8e988291800f4ebdb
Jason Thao <103545293+yogjasonhub@users.noreply.github.com>
2023-03-08 21:09:04 -0600

    Add files via upload

---
+++ PigeonSite/index.js
@@ -0,0 +1,11 @@
+// this is where JS code can be implemented between
+// the <script>
+let nameFirst = 'Jason';
+let pigeonOne = 1;
+let person = {
+    nameFirst: 'Jason',
+    pigeonOne: 1
+}
+console.log(nameFirst);
+console.log(pigeonOne);
+console.log(person);
